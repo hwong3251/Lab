35d6d9a5e75a5c055bc402c7cc8cbfcf534a8d0b
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-11-14 12:52:30 -0500

    productArray complete

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -2,12 +2,16 @@
 public class ArrayMethods extends HelperMethods {
 	public static void main(String[] args)
 	{
-		int[] random = {1,4,1,2,8,9,8,6,3,4,5,1};
-		removeDuplicates(random);
-		//System.out.println(random.length);
+		int[] random = {1,4,1};
+		int[]newArr = removeDuplicates(random);
+		printArray(newArr);
+		
+		int[]arr1 = {1,2,4,6,8};
+		int[]arr2 = {1,2,3};
+		productArray(arr1, arr2);
 	}
 	
-	public static void removeDuplicates(int [] list)
+	public static int[] removeDuplicates(int [] list)
 	{
 		printArray(list);
 		int[]mask = new int[list.length];
@@ -36,7 +40,27 @@ public class ArrayMethods extends HelperMethods {
 				count++;
 			}
 		}
-		printArray(newArr);
+		return newArr;
 	}
-
+	public static int[][]productArray(int[]arr1, int[]arr2)
+	{
+		int[][]newArr = new int[arr1.length][arr2.length];
+		for (int i = 0; i < arr1.length; i++)
+		{
+			for (int j = 0; j < arr2.length; j++)
+			{
+				newArr[i][j] = arr1[i]*arr2[j];
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < arr1.length; i++)
+		{
+			for (int j = 0; j < arr2.length; j++)
+			{
+					System.out.print(newArr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return newArr;
+	}
 }
