6ab71b3e9233cace0eecdbe4a6598f73ca040e79
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-12-01 13:15:38 -0500

    12/1

--- Lab3.4/src/ArrayMethods4.java
+++ Lab3.4/src/ArrayMethods4.java
@@ -1,4 +1,23 @@
 
 public class ArrayMethods4 {
-
+	public static void main(String[]args)
+	{
+		int[]test1 = {1,12,5,26,7,14,3,7,2};
+		int t = partition(test1,0,test1.length);
+		System.out.println(t);
+	}
+	public static int partition(int[]list1, int front, int back)
+	{
+		int pivot = back/2;
+		while(front)
+		return pivot;
+	}
+	public static void quickSort(int[]list1, int front, int back)
+	{
+		
+	}
+	public static void customSort()
+	{
+		
+	}
 }
